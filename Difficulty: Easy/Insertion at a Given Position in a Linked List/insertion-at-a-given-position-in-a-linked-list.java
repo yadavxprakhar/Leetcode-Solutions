877dc38c48dfa96933e90010f7e6506f19269ca2
Prakhar Yadav <137612937+yadavxprakhar@users.noreply.github.com>
2026-04-12 13:39:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion at a Given Position in a Linked List/insertion-at-a-given-position-in-a-linked-list.java
@@ -0,0 +1,39 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertPos(Node head, int pos, int val) {
+        // code here
+        if(head == null){
+            if(pos==1){
+                return new Node(val);
+            }
+        }
+        if( pos==1){
+            Node newNode = new Node(val);
+            newNode.next = head;
+            head = newNode;
+            return head;
+        }
+        int cnt =0;
+        Node temp = head;
+        while(temp != null){
+            cnt++;
+            if(cnt ==pos-1){
+               Node node1 = new Node(val);
+               node1.next= temp.next;
+               temp.next = node1;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+}
